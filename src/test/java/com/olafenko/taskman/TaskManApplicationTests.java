312014d9d8002841e86e4ba4d583a78f364a11bb
olafenko <olopikolo666@gmail.com>
2026-02-22 13:17:03 +0100

    inital commit, create project and add default dependencies

---
+++ src/test/java/com/olafenko/taskman/TaskManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olafenko.taskman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
