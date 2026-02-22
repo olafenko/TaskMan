5fbb74b67f5020cf4fdb98d52704e972fb17601f
olafenko <olopikolo666@gmail.com>
2026-02-22 16:53:27 +0100

    added dependency for spring validation

--- src/main/java/com/olafenko/taskman/models/Task.java
+++ src/main/java/com/olafenko/taskman/models/Task.java
@@ -16,7 +16,7 @@ public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String title;
     private String description;
--- src/main/java/com/olafenko/taskman/models/dtos/CreateTaskRequest.java
+++ src/main/java/com/olafenko/taskman/models/dtos/CreateTaskRequest.java
@@ -2,7 +2,7 @@ package com.olafenko.taskman.models.dtos;
 
 import com.olafenko.taskman.models.enums.TaskPriority;
 import com.olafenko.taskman.models.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
 
-public record CreateTaskRequest(String title, String description, TaskStatus status, TaskPriority priority) {
-
+public record CreateTaskRequest(@NotBlank String title, String description, @NotBlank TaskStatus status, @NotBlank TaskPriority priority) {
 }
