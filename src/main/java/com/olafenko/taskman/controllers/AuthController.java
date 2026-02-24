822240fd8f24b885e7200ac3d06ac10f310149a5
olafenko <olopikolo666@gmail.com>
2026-02-24 16:44:58 +0100

    created AuthController

---
+++ src/main/java/com/olafenko/taskman/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.olafenko.taskman.controllers;
+
+import com.olafenko.taskman.models.dtos.users.RegistrationRequest;
+import com.olafenko.taskman.servicies.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody @Valid RegistrationRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully!");
+    }
+
+    
+}
