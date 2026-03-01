bce8e7f2b4dd3cb61fedcf5c302248b3e1385f41
olafenko <olopikolo666@gmail.com>
2026-03-01 16:04:01 +0100

    started implementing auth filter

--- src/main/java/com/olafenko/taskman/security/JwtAuthFilter.java
+++ src/main/java/com/olafenko/taskman/security/JwtAuthFilter.java
@@ -1,4 +1,38 @@
 package com.olafenko.taskman.security;
 
-public class JwtAuthFilter {
+import com.olafenko.taskman.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String bearerToken = request.getHeader("Authorization");
+
+        if(bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = bearerToken.substring(7);
+
+
+
+    }
 }
